6ac8197b4f91b4579acd38835ff88f4b18957a70
kai-1208 <ra23070n@st.omu.ac.jp>
2026-09-11 12:20:21 +0900

    システム状態によってネオピクの光り方を変更できるようにした。

--- Core/Src/neo-pixel.cpp
+++ Core/Src/neo-pixel.cpp
@@ -27,18 +27,53 @@ public:
     }
 };
 
+static volatile LedSystemState_t current_state = LED_STATE_NORMAL; // 現在のシステム状態を保持する変数
+
+// システム状態を変更する関数
+extern "C" void NeoPixel_SetSystemState(LedSystemState_t new_state) {
+    current_state = new_state;
+}
+
 // C言語側から呼び出すため extern "C" を付与
 extern "C" void StartNeoPixelTask(void *argument)
 {
     // NeoPixelの初期化（TIM1 Channel 3）
     WS2812B_Init(&htim1, TIM_CHANNEL_3);
+
+    bool blink_flag = false;
     
+    // for(;;)
+    // {
+    //     // 全LEDに設定
+    //     WS2812B_SetAll(0, 127, 0);
+    //     WS2812B_Update();
+    //     osDelay(30);
+    // }
+
     for(;;)
     {
-        // 全LEDに設定
-        WS2812B_SetAll(0, 127, 0);
-        WS2812B_Update();
-        osDelay(30);
+        switch(current_state) {
+            case LED_STATE_NORMAL:
+                WS2812B_SetAll(0, 127, 0); // 緑
+                break;
+            case LED_STATE_NET_DISCONNECT:
+                blink_flag = !blink_flag;
+                WS2812B_SetAll(blink_flag ? 127 : 0, 0, 0); // 赤
+                break;
+            case LED_STATE_CAN1_ERROR:
+                blink_flag = !blink_flag;
+                WS2812B_SetAll(blink_flag ? 127 : 0, 127, 0); // 黄
+                break;
+            case LED_STATE_CAN2_ERROR:
+                blink_flag = !blink_flag;
+                WS2812B_SetAll(blink_flag ? 127 : 0, blink_flag ? 63 : 0, 0); // オレンジ
+                break;
+            default:
+                WS2812B_SetAll(0, 0, 0); // 消灯
+                break;
+            WS2812B_Update(); // 状態に応じてLEDを更新
+            osDelay(250); // 30ms待機
+        }
     }
     
     // uint8_t offset = 0;
--- UserLibs/neo-pixel/neo-pixel.h
+++ UserLibs/neo-pixel/neo-pixel.h
@@ -15,10 +15,19 @@ extern "C" {
 #define PWM_HI 180
 #define PWM_LO 90
 
+// システム状態のネオピク表示定義
+typedef enum {
+    LED_STATE_NORMAL = 0,     // 通常動作
+    LED_STATE_NET_DISCONNECT, // ルータ通信遮断
+    LED_STATE_CAN1_ERROR,     // CAN1通信遮断
+    LED_STATE_CAN2_ERROR      // CAN2通信遮断
+} LedSystemState_t;
+
 void WS2812B_Init(TIM_HandleTypeDef *htim, uint32_t channel);
 void WS2812B_SetLED(uint16_t index, uint8_t r, uint8_t g, uint8_t b);
 void WS2812B_SetAll(uint8_t r, uint8_t g, uint8_t b);
 void WS2812B_Update(void);
+void NeoPixel_SetSystemState(LedSystemState_t new_state); // 外部プログラムからシステム状態を設定するための関数
 
 #ifdef __cplusplus
 }
